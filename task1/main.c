135ad25c4ec2749e0eaecbf5d8ab48fe1e60b708
HabroNazar <kn25-n.habro@nubip.edu.ua>
2026-07-04 17:55:30 +0300

    complete task 1

---
+++ task1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double t1, t2, t3, time;
+    printf("Введіть три значення:\n");
+    scanf("%lf %lf %lf", &t1, &t2, &t3);
+    time = 1.0 / (1.0 / t1 + 1.0 / t2 + 1.0 / t3);
+    printf("Час, необхідний для з'їдання пирога: %.2f годин\n", time);
+    return 0;
+}
